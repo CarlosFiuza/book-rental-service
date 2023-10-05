d7ae25821a9ff8cacf809ab5dbdd8bd760d87c57
carlosFiuza <carlosfiuza1234@gmail.com>
2023-10-05 15:08:09 -0300

    fix: adjust test most-delayed-books-by-month

--- src/app/use-cases/most-delayed-books-by-month.spec.ts
+++ src/app/use-cases/most-delayed-books-by-month.spec.ts
@@ -21,6 +21,7 @@ describe('Most delayed books by month', () => {
     const date1 = new Date('2022-03-05');
     const date2 = new Date('2022-03-15');
     const date3 = new Date('2022-03-25');
+    const date4 = new Date('2022-03-29');
 
     await rentRepository.create(
       makeRent({
@@ -46,6 +47,15 @@ describe('Most delayed books by month', () => {
         createdAt: date3,
         expectedReturnAt: new Date('2022-03-26'),
         returnedAt: new Date('2022-03-27'),
+      }),
+    );
+
+    await rentRepository.create(
+      makeRent({
+        replicaId: replicaId1,
+        createdAt: date4,
+        expectedReturnAt: new Date('2022-03-30'),
+        returnedAt: new Date('2022-03-31'),
       }),
     );
 
@@ -105,10 +115,11 @@ describe('Most delayed books by month', () => {
 
     const { statistic } = await mostDelayedBooksByMonth.execute({});
 
-    expect(rentRepository.rents).toHaveLength(9);
+    expect(rentRepository.rents).toHaveLength(10);
     expect(statistic.at(0)?.year).toEqual(2022);
     expect(statistic.at(0)).toHaveProperty('mar');
     expect(statistic.at(0)?.mar).toHaveLength(3);
     expect(statistic.at(0)?.mar?.at(0)?.bookTitle).toEqual(replicaId1);
+    expect(statistic.at(0)?.mar?.at(0)?.num).toEqual(4);
   });
 });
